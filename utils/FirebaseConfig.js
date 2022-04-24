32820f8efc3a1b0fd26f67157a172010b4043aaa
SergioIznaolaDeveloper <86465984+SergioIznaolaDeveloper@users.noreply.github.com>
2022-04-25 00:29:41 +0200

    false_connection_firebase_ignore_fixed

--- utils/FirebaseConfig.js
+++ utils/FirebaseConfig.js
@@ -4,15 +4,15 @@ const { initializeApp } = require("firebase/app");
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAf_XGF-toa6K6fGQ4xl-4IPhb_5M7sW5g",
-  authDomain: "miniapp-1f380.firebaseapp.com",
-  projectId: "miniapp-1f380",
-  storageBucket: "miniapp-1f380.appspot.com",
-  messagingSenderId: "565751507170",
-  appId: "1:565751507170:web:bf2a84af192275fa0d56bb",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
-
 const db = initializeApp(firebaseConfig);
 console.log(db)
+
 // Initialize Firebase
 module.exports = db;
