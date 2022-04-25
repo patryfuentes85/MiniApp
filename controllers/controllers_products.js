3d1935f531c7a038d08c45d1872f325aa22d21d5
SergioIznaolaDeveloper <86465984+SergioIznaolaDeveloper@users.noreply.github.com>
2022-04-25 08:42:13 +0200

    create_styles_views

--- controllers/controllers_products.js
+++ controllers/controllers_products.js
@@ -1,60 +1,64 @@
-const productsEx = require('../models/MongoSchema');
-const { db } = require('../utils/dbMongo');
+const productsEx = require("../models/MongoSchema");
+const { db } = require("../utils/dbMongo");
 
-
-const goHome = async (req,res) => {
-    res.render("home.pug");
-}
-
-
-const getProducts = async (req,res) => {
-    let data;
-    try {
-        if(req.params.name){
-            data = await productsEx.find({name:req.params.name}, ' -_id')
-            console.log(data)
-            res.status(200).json(data);
-        }else{
-            data = await productsEx.find({})
-            res.render("products.pug", { products: data });
-        }
-    } catch (error) {
-        res.status(400).json({"error":error})
-    }
+const goHome = async (req, res) => {
+  res.render("home.pug");
 };
 
+const getProducts = async (req, res) => {
+  let data;
+  try {
+    if (req.params.name) {
+      data = await productsEx.find({ name: req.params.name }, " -_id");
+      console.log(data);
+      res.status(200).json(data);
+    } else {
+      data = await productsEx.find({});
+      res.render("products.pug", { products: data });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
 
-const createProduct = async (req,res) => {
-        try {
-            const newP = req.body;
-            let data = await productsEx.create(newP);
-            res.status(201).json({data})
-        } catch (err) {
-            res.status(400).json({message:err});
-        }
+const createProduct = async (req, res) => {
+  try {
+    const newP = req.body;
+    // let data = await productsEx.create(newP);
+    data = await productsEx.find({});
+    res.render("create_products.pug", { products: data });
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 };
 
 const editProduct = async (req, res) => {
-    try {
-      let query = { id: req.params.name };
-      let update = req.body;
-      const result = await productsEx.findOneAndUpdate(query, update, {
-        new: true,
-        runValidators: true,
-      });
-      res.status(201).json({ message: "modificated product" });
-    } catch (err) {
-      res.status(400).json({ message: err });
-    }
-  };
+  try {
+    let query = { id: req.params.name };
+    let update = req.body;
+    const result = await productsEx.findOneAndUpdate(query, update, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(201).json({ message: "modificated product" });
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+};
 
-  const deleteProduct = async (req, res) => {
-    try {
-      await productsEx.deleteOne({ name: req.params.name })
-      res.status(200).send('deleted Product');
-    } catch (err) {
-      res.status(400).json({ message: err });
-    }
-  };
+const deleteProduct = async (req, res) => {
+  try {
+    await productsEx.deleteOne({ name: req.params.name });
+    res.status(200).send("deleted Product");
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+};
 
-module.exports = {goHome, createProduct, getProducts, editProduct, deleteProduct};
+module.exports = {
+  goHome,
+  createProduct,
+  getProducts,
+  editProduct,
+  deleteProduct,
+};
--- utils/FirebaseConfig.js
+++ utils/FirebaseConfig.js
@@ -2,17 +2,17 @@
 const { initializeApp } = require("firebase/app");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
+// Your web app's Firebase configuratio
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: "AIzaSyAf_XGF-toa6K6fGQ4xl-4IPhb_5M7sW5g",
+  authDomain: "miniapp-1f380.firebaseapp.com",
+  databaseURL: "https://miniapp-1f380-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "miniapp-1f380",
+  storageBucket: "miniapp-1f380.appspot.com",
+  messagingSenderId: "565751507170",
+  appId: "1:565751507170:web:bf2a84af192275fa0d56bb"
 };
 const db = initializeApp(firebaseConfig);
-console.log(db)
 
 // Initialize Firebase
 module.exports = db;
